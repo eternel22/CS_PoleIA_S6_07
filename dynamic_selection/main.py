1b6f2a114e84657c48fde14eaba3c8e04ad3a70a
KillianMcCourt <119616832+KillianMcCourt@users.noreply.github.com>
2023-05-25 10:11:43 +0200

    Update main.py

--- dynamic_selection/main.py
+++ dynamic_selection/main.py
@@ -1,3 +1,9 @@
+import torch
+t = torch.tensor([1.0]) # create tensor with just a 1 in it
+t = t.cuda() # Move t to the gpu
+print(t) # Should print something like tensor([1], device='cuda:0')
+print(t.mean()) # Test an operation just to be sure
+
 from traintools import *
 from utils.args import *
 import warnings
