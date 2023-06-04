b1c95a5bebdcc78025557cec77c53bd173a537aa
KillianMcCourt <119616832+KillianMcCourt@users.noreply.github.com>
2023-06-04 22:10:17 +0200

    Update refinement_dynamic_c10.sh

--- fine/fine-dividemix/scripts/refinement_dynamic_c10.sh
+++ fine/fine-dividemix/scripts/refinement_dynamic_c10.sh
@@ -1,1 +1,1 @@
-python Train_cifar.py --gpuid 0 --noise_mode sym --r 0.6 --lambda_u 150 --dataset cifar10 --num_class 10  --p_threshold 0.6 --distill_mode fine-gmm  --distill dynamic --data_path ./cifar-10
+python Train_cifar.py --gpuid 0 --noise_mode sym --r 0.4 --lambda_u 150 --dataset cifar10 --num_class 10  --p_threshold 0.6 --distill_mode fine-gmm  --distill dynamic --data_path ./cifar-10
