d467beac58e2a032d6cba0f2d50e7d88b6351762
KillianMcCourt <119616832+KillianMcCourt@users.noreply.github.com>
2023-06-04 13:19:37 +0200

    Update fine_cifar.sh

--- fine/dynamic_selection/scripts/sample_selection_based/fine_cifar.sh
+++ fine/dynamic_selection/scripts/sample_selection_based/fine_cifar.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python main.py -d 1 --asym false --percent 0.8 --lr_scheduler multistep --arch rn18 --loss_fn cce --dataset cifar10 --traintools robustloss --no_wandb --dynamic --distill_mode fine-gmm --seed 123 --warmup 40
+python main.py -d 1 --asym false --percent 0.6 --lr_scheduler multistep --arch rn18 --loss_fn cce --dataset cifar10 --traintools robustloss --no_wandb --dynamic --distill_mode fine-gmm --seed 123 --warmup 40
